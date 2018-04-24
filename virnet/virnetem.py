0ef18106ed52b8ac1538b3ef7cc5f62577b04a38
Wang Xintian <wxintian@163.com>
2018-04-24 17:59:52 +0800

    add ms virnet

--- virnet/virnetem.py
+++ virnet/virnetem.py
@@ -44,47 +44,47 @@ class MsHander(SocketServer.StreamRequestHandler):
         self.logout()
 
     def login(self):
-        print '...connected from:', self.client_address
+        logging.debug('...connected from:%s', self.client_address)
         # reqMsg = self.rfile.readline()
         buff = self.request.recv(38)
-        print 'login:%s' % repr(buff)
+        logging.debug('login:%s', repr(buff))
         reqLen = len(buff)
-        print 'login package length:%d ' % reqLen
+        logging.debug('login package length:%d ', reqLen)
         fLength, fMsgType, fSquence, fClientId, fUser, fPasswd = struct.unpack(self.loginFmt, buff)
-        print 'login:%d %d %d %d %s %s' % (fLength, fMsgType, fSquence, fClientId, fUser, fPasswd)
+        logging.debug('login:%d %d %d %d %s %s', fLength, fMsgType, fSquence, fClientId, fUser, fPasswd)
         loginStatus = 0
         fSquence = 1
         fClientId = 2
         fRspLength = 27
         buffRsp = struct.pack(self.loginRspFmt, fRspLength, fMsgType, fSquence, fClientId, fUser, loginStatus)
-        print 'login resp:%d %d %d %d %s %d' % (fRspLength, fMsgType, fSquence, fClientId, fUser, loginStatus)
-        print 'login resp:%s' % repr(buffRsp)
+        logging.debug('login resp:%d %d %d %d %s %d', fRspLength, fMsgType, fSquence, fClientId, fUser, loginStatus)
+        logging.debug('login resp:%s', repr(buffRsp))
         self.request.sendall(buffRsp)
 
     def doCmd(self):
         buff = self.request.recv(14)
-        print 'cmd head:%s' % repr(buff)
+        logging.debug('cmd head:%s', repr(buff))
         fLength, fMsgType, fSquence, fClientId = struct.unpack(self.headFmt, buff)
-        print 'cmd head:%d %d %d %d' % (fLength, fMsgType, fSquence, fClientId)
+        logging.debug('cmd head:%d %d %d %d', fLength, fMsgType, fSquence, fClientId)
         cmdLengtgh = fLength - 14
         cmdFmt = self.cmdBodyFmt % cmdLengtgh
         buff = self.request.recv(cmdLengtgh)
-        print 'cmd:%s' % repr(buff)
+        logging.debug('cmd:%s', repr(buff))
         cmd = struct.unpack(cmdFmt, buff)
-        print 'request:%d %d %d %d %s' % (fLength, fMsgType, fSquence, fClientId, cmd)
+        logging.debug('request:%d %d %d %d %s', fLength, fMsgType, fSquence, fClientId, cmd)
         rspBuff = struct.pack(self.cmdRspFmt, 15, fMsgType, fSquence, fClientId, 0)
-        print 'response:%d %d %d %d %d' % (15, fMsgType, fSquence, fClientId, 0)
-        print 'cmd resp:%s' % repr(rspBuff)
+        logging.debug('response:%d %d %d %d %d', 15, fMsgType, fSquence, fClientId, 0)
+        logging.debug('cmd resp:%s', repr(rspBuff))
         self.request.sendall(rspBuff)
 
     def logout(self):
         buff = self.request.recv(14)
-        print 'logout:%s' % repr(buff)
+        logging.debug('logout:%s', repr(buff))
         fLength, fMsgType, fSquence, fClientId = struct.unpack(self.headFmt, buff)
-        print 'request:%d %d %d %d' % (fLength, fMsgType, fSquence, fClientId)
+        logging.debug('request:%d %d %d %d', fLength, fMsgType, fSquence, fClientId)
         rspBuff = struct.pack(self.cmdRspFmt, 15, fMsgType, fSquence, fClientId, 0)
-        print 'response:%d %d %d %d %d' % (15, fMsgType, fSquence, fClientId, 0)
-        print 'logout resp:%s' % repr(rspBuff)
+        logging.debug('response:%d %d %d %d %d', 15, fMsgType, fSquence, fClientId, 0)
+        logging.debug('logout resp:%s', repr(rspBuff))
         self.request.sendall(rspBuff)
 
 
@@ -553,6 +553,18 @@ class Main(object):
         self.fNet = None
         self.netFile = None
 
+    def checkArgv(self):
+        dirBin, appName = os.path.split(self.Name)
+        self.appName = appName
+        if self.argc < 2:
+            self.usage()
+        self.netFile = sys.argv[1]
+
+    def usage(self):
+        print "Usage: %s netcfg" % self.appName
+        print "example:   %s %s" % (self.appName,'hsshwcfg')
+        exit(1)
+
     def parseWorkEnv(self):
         dirBin, appName = os.path.split(self.Name)
         self.dirBin = dirBin
@@ -595,18 +607,6 @@ class Main(object):
         self.logFile = os.path.join(self.dirLog, logName)
         self.logPre = os.path.join(self.dirLog, logNamePre)
         # self.outFile = os.path.join(self.dirOutput, outFileName)
-
-    def checkArgv(self):
-        dirBin, appName = os.path.split(self.Name)
-        self.appName = appName
-        if self.argc < 2:
-            self.usage()
-        self.netFile = sys.argv[1]
-
-    def usage(self):
-        print "Usage: %s netcfg" % self.appName
-        print "example:   %s %s" % (self.appName,'hsshwcfg')
-        exit(1)
 
     def openFile(self, fileName, mode):
         try:
